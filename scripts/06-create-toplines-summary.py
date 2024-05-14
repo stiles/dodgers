0576cb4c2200e9b1477fdf4bef5cc4be89998b00
Matt Stiles <mattstiles@gmail.com>
2024-05-14 12:06:16 -0700

    better summary stats

--- scripts/06-create-toplines-summary.py
+++ scripts/06-create-toplines-summary.py
@@ -18,6 +18,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 # Base directory calculation for file paths
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(base_dir)
 
 def read_parquet_s3(url, sort_by=None):
     """Read a Parquet file from the S3 URL.
@@ -31,6 +32,9 @@ def read_parquet_s3(url, sort_by=None):
 # URLs for data
 standings_url = "https://stilesdata.com/dodgers/data/standings/dodgers_standings_1958_present.parquet"
 batting_url = "https://stilesdata.com/dodgers/data/batting/dodgers_team_batting_1958_present.parquet"
+pitching_url = 'https://stilesdata.com/dodgers/data/pitching/dodgers_pitching_totals_current.parquet'
+pitching_ranks_url = 'https://stilesdata.com/dodgers/data/pitching/dodgers_pitching_ranks_current.parquet'
+batting_ranks_url = 'https://stilesdata.com/dodgers/data/batting/dodgers_team_batting_ranks_1958_present.parquet'
 
 mlb_teams = {
     "ARI": "Arizona Diamondbacks",
@@ -66,56 +70,84 @@ mlb_teams = {
 }
 
 # Load the data
+
+# Standings
 standings = read_parquet_s3(standings_url, sort_by='game_date').query("year == '2024'")
 standings['result'] = standings['result'].str.split('-wo', expand=True)[0]
-standings['opp_name'] = standings['opp'].map(mlb_teams)
 standings.loc[standings.result == "L", "result_clean"] = "loss"
 standings.loc[standings.result == "W", "result_clean"] = "win"
 standings_past = read_parquet_s3(standings_url, sort_by='game_date').query("year != '2024'")
 standings_now = standings.query("game_date == game_date.max()").copy()
+game_number = standings_now['gm'].iloc[0]
+standings_last = standings_past.query(f"gm == {game_number}").head(1).reset_index(drop=True).copy()
+standings_last_season = standings_past.query(f"gm <= {game_number} and year=='2023'").reset_index(drop=True).copy()
 
+# Batting
 batting = read_parquet_s3(batting_url)
 batting_past = batting.query("season != '2024'").copy()
 batting_now = batting.query("season == '2024'").copy()
+batting_ranks = read_parquet_s3(batting_ranks_url, sort_by='game_date').query("season == '2024'")
 
-def current_season_stats(standings_now, standings_past):
+# Pitching
+pitching = read_parquet_s3(pitching_url)
+pitching_ranks = read_parquet_s3(pitching_ranks_url)
+
+def current_season_stats(standings_now, standings_past, pitching, pitching_ranks, standings_last):
     games = standings_now["gm"].iloc[0]
     wins = standings_now["wins"].iloc[0]
+    wins_last = standings_last["wins"].iloc[0]
     losses = standings_now["losses"].iloc[0]
+    losses_last = standings_last["losses"].iloc[0]
     record = standings_now["record"].iloc[0]
+    record_last = standings_last["record"].iloc[0]
     win_pct = int(standings_now["win_pct"].iloc[0] * 100)
+    win_pct_last = int(standings_last["win_pct"].iloc[0] * 100)
     win_pct_decade_thispoint = int(
         standings_past.query(f"gm == {games}").head(10)["win_pct"].mean().round(2) * 100
     )
-    return games, wins, losses, record, win_pct, win_pct_decade_thispoint
+    era = pitching['era'].iloc[0]
+    era_rank = pitching_ranks['era'].iloc[0]
+    strikeouts = pitching['so'].iloc[0]
+    strikeouts_rank = pitching_ranks['so'].iloc[0]
+    walks = pitching['bb'].iloc[0]
+    walks_rank = pitching_ranks['bb'].iloc[0]
+    home_runs_allowed = pitching['hr'].iloc[0]
+    home_runs_allowed_rank = pitching_ranks['hr'].iloc[0]
 
-def run_differential(standings):
+    return games, wins, losses, record, win_pct, win_pct_decade_thispoint, era, era_rank, strikeouts, strikeouts_rank, walks, walks_rank, wins_last, losses_last, record_last, win_pct_last, home_runs_allowed, home_runs_allowed_rank
+
+def run_differential(standings, batting_ranks):
     runs = standings["r"].sum()
+    runs_last = standings_last_season['r'].sum()
+    runs_rank = batting_ranks['r'].iloc[0]
     runs_against = standings["ra"].sum()
+    runs_against_last = standings_last_season['ra'].sum()
     run_diff = runs - runs_against
-    return runs, runs_against, run_diff
+    run_diff_last = runs_last - runs_against_last
+    return runs, runs_last, runs_rank, runs_against, runs_against_last, run_diff, run_diff_last
 
-def home_run_stats(batting_now, batting_past):
+def home_run_stats(batting_now, batting_past, batting_ranks):
     games = int(batting_now["g"].iloc[0])
     home_runs = int(batting_now["hr"].sum())
+    home_runs_rank = batting_ranks['hr'].iloc[0]
     home_runs_game = round(home_runs / games, 2)
     batting_past["hr_game"] = batting_past["hr"].astype(int) / batting_past["g"].astype(int).round(2)
     home_runs_game_last = batting_past.query('season == "2023"')["hr_game"].iloc[0]
     games_decade = batting_past.head(10)["g"].astype(int).sum()
     home_runs_decade = batting_past.head(10)["hr"].astype(int).sum()
     home_runs_game_decade = round(home_runs_decade / games_decade, 2)
-    return home_runs, home_runs_game, home_runs_game_last, home_runs_game_decade
+    return home_runs, home_runs_game, home_runs_game_last, home_runs_game_decade, home_runs_rank
 
-def batting_and_stolen_base_stats(batting_now, batting_past, games):
+def batting_and_stolen_base_stats(batting_now, batting_past, games, batting_ranks):
     batting_average = batting_now["ba"].iloc[0]
     batting_average_decade = round(
         batting_past.head(10)["ba"].astype(float).mean(), 3
     ).astype(str).replace("0.", ".")
     stolen_bases = int(batting_now["sb"].iloc[0])
+    stolen_bases_rank = batting_ranks['sb'].iloc[0]
     stolen_bases_game = round(stolen_bases / games, 2)
-    stolen_decade = batting_past.head(10)["sb"].astype(int).sum()
-    stolen_bases_decade_game = round(stolen_decade / games, 2)
-    return batting_average, batting_average_decade, stolen_bases, stolen_bases_game, stolen_bases_decade_game
+    stolen_bases_last_rate = round(batting_past.head(1)["sb"].astype(int).sum() / batting_past.head(1)["g"].astype(int).sum(), 2)
+    return batting_average, batting_average_decade, stolen_bases, stolen_bases_rank, stolen_bases_game, stolen_bases_last_rate
 
 def generate_summary(standings_now, wins, losses, win_pct):
     last_game = standings_now.iloc[0]
@@ -134,34 +166,33 @@ def recent_trend(standings):
     loss_count_trend = last_10[last_10 == "L"].count()
     return win_count_trend, loss_count_trend, f"Recent trend: {win_count_trend} wins, {loss_count_trend} losses"
 
-games, wins, losses, record, win_pct, win_pct_decade_thispoint = current_season_stats(standings_now, standings_past)
-runs, runs_against, run_diff = run_differential(standings)
-home_runs, home_runs_game, home_runs_game_last, home_runs_game_decade = home_run_stats(batting_now, batting_past)
-batting_average, batting_average_decade, stolen_bases, stolen_bases_game, stolen_bases_decade_game = batting_and_stolen_base_stats(batting_now, batting_past, games)
+games, wins, losses, record, win_pct, win_pct_decade_thispoint, era, era_rank, strikeouts, strikeouts_rank, walks, walks_rank, wins_last, losses_last, record_last, win_pct_last, home_runs_allowed, home_runs_allowed_rank = current_season_stats(standings_now, standings_past, pitching, pitching_ranks, standings_last)
+runs, runs_last, runs_rank, runs_against, runs_against_last, run_diff, run_diff_last = run_differential(standings, batting_ranks)
+home_runs, home_runs_game, home_runs_game_last, home_runs_game_decade, home_runs_rank = home_run_stats(batting_now, batting_past, batting_ranks)
+batting_average, batting_average_decade, stolen_bases, stolen_bases_rank, stolen_bases_game, stolen_bases_last_rate = batting_and_stolen_base_stats(batting_now, batting_past, games, batting_ranks)
 win_count_trend, loss_count_trend, win_loss_trend = recent_trend(standings.iloc[:10])
 
 summary = generate_summary(standings, wins, losses, win_pct)
 
 summary_data = [
-    {"stat_label": "Wins", "stat": "wins", "value": wins, "category": "standings"},
-    {"stat_label": "Losses", "stat": "losses", "value": losses, "category": "standings"},
-    {"stat_label": "Record", "stat": "record", "value": record, "category": "standings"},
-    {"stat_label": "Win %", "stat": "win_pct", "value": f"{win_pct}%", "category": "standings"},
-    {"stat_label": "Win % last decade", "stat": "win_pct_decade_thispoint", "value": f"{win_pct_decade_thispoint}%", "category": "standings"},
-    {"stat_label": "Runs", "stat": "runs", "value": runs, "category": "standings"},
-    {"stat_label": "Runs against", "stat": "runs_against", "value": runs_against, "category": "standings"},
-    {"stat_label": "Run differential", "stat": "run_differential", "value": run_diff, "category": "standings"},
-    {"stat_label": "Home runs", "stat": "home_runs", "value": home_runs, "category": "batting"},
-    {"stat_label": "HR/game", "stat": "home_runs_game", "value": home_runs_game, "category": "batting"},
-    # {"stat_label": "HR/game 2023", "stat": "home_runs_game_last", "value": home_runs_game_last, "category": "batting"},
-    {"stat_label": "HR/game last decade", "stat": "home_runs_game_decade", "value": home_runs_game_decade, "category": "batting"},
-    {"stat_label": "Stolen bases", "stat": "stolen_bases", "value": stolen_bases, "category": "batting"},
-    {"stat_label": "Stolen/game", "stat": "stolen_bases_game", "value": stolen_bases_game, "category": "batting"},
-    # {"stat_label": "Stolen/game 2023", "stat": "stolen_bases_decade_game", "value": stolen_bases_decade_game, "category": "batting"},
-    {"stat_label": "Batting average", "stat": "batting_average", "value": batting_average, "category": "batting"},
-    {"stat_label": "Batting average decade", "stat": "batting_average_decade", "value": batting_average_decade, "category": "batting"},
-    # {"stat_label": "Recent trend", "stat": "recent_trend", "value": win_loss_trend, "category": "standings"},
-    {"stat_label": "Team summary", "stat": "summary", "value": summary, "category": "summary"}
+    {"stat_label": "Wins", "stat": "wins", "value": wins, "category": "standings", "context_value": wins_last, "context_value_label": "This point last season"},
+    {"stat_label": "Losses", "stat": "losses", "value": losses, "category": "standings", "context_value": losses_last, "context_value_label": "This point last season"},
+    {"stat_label": "Record", "stat": "record", "value": record, "category": "standings", "context_value": record_last, "context_value_label": "This point last season"},
+    {"stat_label": "Win percentage", "stat": "win_pct", "value": f"{win_pct}%", "category": "standings", "context_value": f"{win_pct_last}%", "context_value_label": "This point last season"},
+    {"stat_label": "Runs", "stat": "runs", "value": runs, "category": "standings", "context_value": runs_rank, "context_value_label": "League rank"},
+    {"stat_label": "Runs against", "stat": "runs_against", "value": runs_against, "category": "standings", "context_value": runs_against_last, "context_value_label": "This point last season"},
+    {"stat_label": "Run differential", "stat": "run_differential", "value": run_diff, "category": "standings", "context_value": run_diff_last, "context_value_label": "This point last season"},
+    {"stat_label": "Home runs", "stat": "home_runs", "value": home_runs, "category": "batting", "context_value": home_runs_rank, "context_value_label": "League rank"},
+    {"stat_label": "Home runs/game", "stat": "home_runs_game", "value": home_runs_game, "category": "batting", "context_value": home_runs_game_decade, "context_value_label": "Last decade average"},
+    {"stat_label": "Stolen bases", "stat": "stolen_bases", "value": stolen_bases, "category": "batting", "context_value": stolen_bases_rank, "context_value_label": "League rank"},
+    {"stat_label": "Stolen bases per game", "stat": "stolen_bases_game", "value": stolen_bases_game, "category": "batting", "context_value": stolen_bases_last_rate, "context_value_label": "Rate all last season"},
+    {"stat_label": "Batting average", "stat": "batting_average", "value": batting_average, "category": "batting", "context_value": batting_average_decade, "context_value_label": "Last decade average"},
+    {"stat_label": "ERA", "stat": "era", "value": era, "category": "pitching", "context_value_label": "Rank", "context_value": era_rank, "context_value_label": "League rank"},
+    {"stat_label": "Strikeouts", "stat": "strikeouts", "value": strikeouts, "category": "pitching", "context_value": strikeouts_rank, "context_value_label": "League rank"},
+    {"stat_label": "Walks", "stat": "walks", "value": walks, "category": "pitching", "context_value_label": "Rank", "context_value": walks_rank, "context_value_label": "League rank"},
+    {"stat_label": "Home runs allowed", "stat": "home_runs_allowed", "value": home_runs_allowed, "category": "pitching", "context_value": home_runs_allowed_rank, "context_value_label": "League rank"},
+    {"stat_label": "Recent trend", "stat": "recent_trend", "value": win_loss_trend, "category": "standings", "context_value": "", "context_value_label": ''},
+    {"stat_label": "Team summary", "stat": "summary", "value": summary, "category": "standings", "context_value": "", "context_value_label": ''}
 ]
 
 summary_df = pd.DataFrame(summary_data)
