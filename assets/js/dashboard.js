bd99d2017ea24b309f705be1cddc4cca6ef595f6
stiles <stiles@users.noreply.github.com>
2025-03-30 15:55:04 +0000

    deploy: a3243fb67e829f2109ff435ffd19d4bfcd77fb69

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1,4 +1,5 @@
 
+
 // Games back line chart
 
 async function fetchData() {
@@ -6,8 +7,8 @@ async function fetchData() {
     const response = await d3.json(
       'https://stilesdata.com/dodgers/data/standings/dodgers_standings_1958_present_optimized.json'
     );
-    // Group data by year
-    const groupedByYear = d3.group(response, (d) => d.year);
+    // Group data by year, converting the year to a string for consistency
+    const groupedByYear = d3.group(response, (d) => d.year.toString());
     renderChart(groupedByYear);
   } catch (error) {
     console.error('Failed to fetch data:', error);
@@ -15,6 +16,8 @@ async function fetchData() {
 }
 
 function renderChart(data) {
+  // Dynamically get the current year as a string
+  const currentYear = new Date().getFullYear().toString();
   const isMobile = window.innerWidth <= 767; // Example breakpoint for mobile devices
   const margin = isMobile 
     ? { top: 20, right: 40, bottom: 60, left: 50 }  // Smaller margins for mobile
@@ -23,8 +26,8 @@ function renderChart(data) {
   const containerWidth = container.node().getBoundingClientRect().width;
   const width = containerWidth - margin.left - margin.right;
   const height = isMobile 
-  ? Math.round(width * 1) - margin.top - margin.bottom  // Taller for mobile
-  : Math.round(width * 0.5) - margin.top - margin.bottom; // 2x1 ratio for desktop
+    ? Math.round(width * 1) - margin.top - margin.bottom  // Taller for mobile
+    : Math.round(width * 0.5) - margin.top - margin.bottom; // 2x1 ratio for desktop
 
   const svg = container
     .append('svg')
@@ -32,73 +35,64 @@ function renderChart(data) {
     .append('g')
     .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-    const xScale = d3
-    .scaleLinear()
+  const xScale = d3.scaleLinear()
     .domain([0, 166])
     .range([0, width]);
 
-  const yScale = d3
-    .scaleLinear()
+  const yScale = d3.scaleLinear()
     .domain([
       d3.min(Array.from(data.values()).flat(), (d) => d.gb),
-      d3.max(Array.from(data.values()).flat(), (d) => d.gb),
+      d3.max(Array.from(data.values()).flat(), (d) => d.gb)
     ])
     .range([height, 0]);
 
-    const xAxis = d3.axisBottom(xScale).ticks(6).tickFormat(d3.format('d'));
-    const yAxis = d3.axisLeft(yScale).ticks(6);
+  const xAxis = d3.axisBottom(xScale).ticks(6).tickFormat(d3.format('d'));
+  const yAxis = d3.axisLeft(yScale).ticks(6);
   
-    svg
-      .append('g')
-      .attr('transform', `translate(0, ${height})`)
-      .call(xAxis);
+  svg.append('g')
+    .attr('transform', `translate(0, ${height})`)
+    .call(xAxis);
   
-    svg.append('g').call(yAxis);
+  svg.append('g').call(yAxis);
   
-    // X-axis Label
-    svg.append("text")
-      .attr("text-anchor", "middle")
-      .attr('class', 'anno-dark')
-      .attr("x", width / 2)
-      .attr("y", height + margin.bottom - 10)
-      .text("Game number in season");
+  // X-axis Label
+  svg.append("text")
+    .attr("text-anchor", "middle")
+    .attr('class', 'anno-dark')
+    .attr("x", width / 2)
+    .attr("y", height + margin.bottom - 10)
+    .text("Game number in season");
   
-    // Y-axis Label
-    svg.append("text")
-      .attr("text-anchor", "middle")
-      .attr('class', 'anno-dark')
-      .attr("transform", "rotate(-90)")
-      .attr("y", -margin.left + 20)
-      .attr("x", -height / 2)
-      .text("Games up/back");
+  // Y-axis Label
+  svg.append("text")
+    .attr("text-anchor", "middle")
+    .attr('class', 'anno-dark')
+    .attr("transform", "rotate(-90)")
+    .attr("y", -margin.left + 20)
+    .attr("x", -height / 2)
+    .text("Games up/back");
 
   // Append axes to SVG
-  svg
-    .append('g')
+  svg.append('g')
     .attr('transform', `translate(0, ${height})`)
     .call(xAxis)
-    .selectAll('line') // Select all lines which includes ticks and optionally grid lines
-    .style('stroke', '#ddd'); // Light gray for a softer look
+    .selectAll('line')
+    .style('stroke', '#ddd');
 
   svg.append('g').call(yAxis).selectAll('line').style('stroke', '#ddd');
+  svg.selectAll('.domain').style('stroke', '#e9e9e9');
 
-  svg
-    .selectAll('.domain') // This selects the domain line of the axes
-    .style('stroke', '#e9e9e9'); // Light grey color
-
-  const line = d3
-    .line()
+  const line = d3.line()
     .x((d) => xScale(d.gm))
     .y((d) => yScale(d.gb))
-    .curve(d3.curveMonotoneX); // Smooth the line
+    .curve(d3.curveMonotoneX);
 
-  // Draw all lines except 2024 first
-  const allLinesExcept2024 = Array.from(data.entries()).filter(
-    (d) => d[0] !== '2024'
+  // Draw all lines except the current year first
+  const allLinesExceptCurrent = Array.from(data.entries()).filter(
+    (d) => d[0] !== currentYear
   );
-  svg
-    .selectAll('.line')
-    .data(allLinesExcept2024, (d) => d[0])
+  svg.selectAll('.line')
+    .data(allLinesExceptCurrent, (d) => d[0])
     .enter()
     .append('path')
     .attr('class', 'line')
@@ -107,10 +101,10 @@ function renderChart(data) {
     .style('stroke', '#ccc')
     .style('stroke-width', 0.5);
 
-  const line2024 = Array.from(data.entries()).filter((d) => d[0] === '2024');
-  svg
-    .selectAll('.line-2024')
-    .data(line2024, (d) => d[0])
+  // Draw the current year line
+  const lineCurrent = Array.from(data.entries()).filter((d) => d[0] === currentYear);
+  svg.selectAll('.line-current')
+    .data(lineCurrent, (d) => d[0])
     .enter()
     .append('path')
     .attr('class', 'line')
@@ -120,19 +114,17 @@ function renderChart(data) {
     .style('stroke-width', 2);
 
   // Add a horizontal line at y = 0
-  svg
-    .append('line')
+  svg.append('line')
     .attr('x1', 0)
-    .attr('x2', isMobile ? width-7 : width-18)
+    .attr('x2', isMobile ? width - 7 : width - 18)
     .attr('y1', yScale(0))
     .attr('y2', yScale(0))
     .attr('stroke', '#222')
     .attr('stroke-width', 1);
 
   // Add the 'Leading' annotation
-  svg
-    .append('text')
-    .attr('x', isMobile ? xScale(75) : xScale(70)) // Adjusted for mobile
+  svg.append('text')
+    .attr('x', isMobile ? xScale(75) : xScale(70))
     .attr('y', yScale(0) - 10)
     .text('Leading ↑')
     .attr('class', 'anno-dark')
@@ -144,58 +136,49 @@ function renderChart(data) {
     .clone(true)
     .style('stroke', 'none');
 
-  // Add the 'Past ' annotation
-  svg
-    .append('text')
-    .attr('x', isMobile ? xScale(80) : xScale(110)) // Adjusted for mobile
+  // Add the 'Past' annotation
+  svg.append('text')
+    .attr('x', isMobile ? xScale(80) : xScale(110))
     .attr('y', yScale(22))
     .attr('class', 'anno')
-    .text('Past: 1958-2023')
+    .text(`Past: 1958-${parseInt(currentYear) - 1}`)
     .attr('text-anchor', 'start');
 
-  const lastData2024 = data.get('2024').slice(0)[0];
-  // console.log(lastData2024);
+  // Get the last data point for the current year safely
+  const currentDataArray = data.get(currentYear);
+  if (currentDataArray && currentDataArray.length > 0) {
+    const lastDataCurrent = currentDataArray.slice(-1)[0];
+    svg.append('text')
+      .attr('x', xScale(lastDataCurrent.gm + 1))
+      .attr('y', yScale(lastDataCurrent.gb) - 12)
+      .text(currentYear)
+      .attr('class', 'anno-dodgers')
+      .style('stroke', '#fff')
+      .style('stroke-width', '4px')
+      .style('stroke-linejoin', 'round')
+      .attr('text-anchor', 'start')
+      .style('paint-order', 'stroke')
+      .clone(true)
+      .style('stroke', 'none');
 
-  svg
-    .append('text')
-    .attr('x', xScale(lastData2024.gm + 1))
-    .attr('y', yScale(lastData2024.gb) - 12)
-    .text('2024')
-    .attr('class', 'anno-dodgers')
-    .style('stroke', '#fff')
-    .style('stroke-width', '4px')
-    .style('stroke-linejoin', 'round')
-    .attr('text-anchor', 'start')
-    .style('paint-order', 'stroke')
-    .clone(true)
-    .style('stroke', 'none');
-
-  function gameStatusText(gamesBack) {
-    if (gamesBack > 0) {
-      return gamesBack + ' games up';
-    } else if (gamesBack < 0) {
-      return -gamesBack + ' games back';
-    } else {
-      return ' games back';
-    }
+    svg.append('text')
+      .attr('x', xScale(lastDataCurrent.gm + 1))
+      .attr('y', yScale(lastDataCurrent.gb) + 2)
+      .text(`${lastDataCurrent.gb} games`)
+      .attr('class', 'anno-dark')
+      .style('stroke', '#fff')
+      .style('stroke-width', '4px')
+      .style('stroke-linejoin', 'round')
+      .attr('text-anchor', 'start')
+      .style('paint-order', 'stroke')
+      .clone(true)
+      .style('stroke', 'none');
   }
-
-  svg
-    .append('text')
-    .attr('x', xScale(lastData2024.gm + 1))
-    .attr('y', yScale(lastData2024.gb) + 2)
-    .text(gameStatusText(lastData2024.gb))
-    .attr('class', 'anno-dark')
-    .style('stroke', '#fff')
-    .style('stroke-width', '4px')
-    .style('stroke-linejoin', 'round')
-    .attr('text-anchor', 'start')
-    .style('paint-order', 'stroke')
-    .clone(true)
-    .style('stroke', 'none');
 }
 
 fetchData();
+
+
 
 
 
@@ -226,14 +209,20 @@ function renderRunDiffChart(data) {
     .append('g')
     .attr('transform', `translate(${margin.left},${margin.top})`);
 
-  const xScale = d3.scaleBand()
+  const xScale = d3
+    .scaleBand()
     .range([0, width])
     .padding(0.1)
-    .domain(data.map(d => d.gm));
+    .domain(d3.range(1, 163));
 
-  const yScale = d3.scaleLinear()
-    .range([height, 0])
-    .domain([d3.min(data, d => d.run_diff), d3.max(data, d => d.run_diff)]);
+    const minRunDiff = d3.min(data, d => d.run_diff);
+    const maxRunDiff = d3.max(data, d => d.run_diff);
+    // Ensure 0 is included in the domain
+    const yDomain = [Math.min(0, minRunDiff), Math.max(0, maxRunDiff)];
+    
+    const yScale = d3.scaleLinear()
+      .range([height, 0])
+      .domain(yDomain);
 
   const xAxis = d3.axisBottom(xScale).tickValues(xScale.domain().filter(d => d % 20 === 0));
   const yAxis = d3.axisLeft(yScale).ticks(5);
@@ -273,9 +262,6 @@ function renderRunDiffChart(data) {
   
 }
 fetchGameData();
-
-
-
 
 document.addEventListener('DOMContentLoaded', function() {
   let groupedByYear;
