d805e0e23133810940ddda65444007278ebcf829
stiles <mattstiles@gmail.com>
2025-03-31 17:01:37 -0700

    tinkering

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -1324,10 +1324,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function renderMaxAttendanceInfo(data) {
     const maxAttendanceTeam = data.reduce((max, team) => (team.attend_game > max.attend_game ? team : max), data[0]);
-    const maxAttendanceText = `The average attendance to see the ${maxAttendanceTeam.team} at ${maxAttendanceTeam.name} so far this season is ${maxAttendanceTeam.attend_game.toLocaleString()}, more than any other franchise in Major League Baseball.`;
+    const maxAttendanceText = `The average attendance to see the ${maxAttendanceTeam.team} at ${maxAttendanceTeam.name} so far this season is <span class='win'>${maxAttendanceTeam.attend_game.toLocaleString()}</span>, more than any other franchise in Major League Baseball.`;
 
     // Insert the text into a paragraph element
-    d3.select('#max-attendance-info').text(maxAttendanceText);
+    d3.select('#max-attendance-info').html(maxAttendanceText);
   }
 
   fetchTableData();
