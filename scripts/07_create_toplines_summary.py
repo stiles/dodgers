394a1ca1998f90dfadd707639a4b610db5aa9ff6
Matt Stiles <mattstiles@gmail.com>
2025-09-26 17:23:10 -0700

    text change

--- scripts/07_create_toplines_summary.py
+++ scripts/07_create_toplines_summary.py
@@ -521,7 +521,7 @@ def generate_summary(
 
     summary = (
         f"<span class='highlight'>LOS ANGELES</span> <span class='updated'>({update_date_str})</span> — "
-        f"After <span class='highlight'>{games_played}</span> games this season, the Dodgers havecompiled a <span class='highlight'>{record}</span> record, winning <span class='highlight'>{win_pct:.0f}%</span> of its games so far and are the National League West division champs."
+        f"After <span class='highlight'>{games_played}</span> games this season, the Dodgers have compiled a <span class='highlight'>{record}</span> record, winning <span class='highlight'>{win_pct:.0f}%</span> of its games. They are the National League West division champs!"
         f"{last_game_summary_fragment} "
         f"They've won <span class='highlight'>{last_10_wins} of the last 10</span>."
     )
